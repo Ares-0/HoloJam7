5431d65cfb04487f849100b0c62ad14bb1cf0ef1
CE-0 <93526532+CE-0@users.noreply.github.com>
2026-01-10 19:07:01 +0100

    Update card_discard_menu.gd

--- source/gui/menus/card_discard_menu.gd
+++ source/gui/menus/card_discard_menu.gd
@@ -8,11 +8,6 @@ var card_option = preload("res://source/gui/card_option.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GameManager.card_discard_menu = self
-	for x in range(0, len(Global.deck)):
-		var card = card_option.instantiate() 
-		deck_grid.add_child(card)
-		card.deck_idx = x 
-		card.card_img.setup_from_card_num(Global.deck[x])
 
 func update_grid(): 
 	clear_children(deck_grid) 
@@ -23,8 +18,11 @@ func update_grid():
 		card.card_img.setup_from_card_num(Global.deck[x])
 
 func clear_children(node):
-	for c in node.get_children():
-		c.queue_free()
+	if (node.get_child_count() > 0):
+		for c in node.get_children():
+			c.queue_free()
+	else: 
+		return
 
 func remove_selection():
 	for x in range(0, deck_grid.get_child_count()):
