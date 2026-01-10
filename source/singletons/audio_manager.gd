56673e6ae34f3b3853e990291d9cd2fde81c16df
Ares <mason.hoppe@gmail.com>
2026-01-10 02:16:34 -0500

    Most basic music logic ever

--- source/singletons/audio_manager.gd
+++ source/singletons/audio_manager.gd
@@ -3,38 +3,51 @@ extends Node
 # Autoload singelton that plays the music and sound effects of the game
 
 var current_ost: NodePath
+var ost_on: bool = false
 
 func _ready() -> void:
 	#print("audio ready")
-	#start ost
 	pass
-	#for child in get_children():
-		#print("playing ", child.name)
-		#child.play()
-		#await child.finished
+	soundtrack_start()
 
 func _process(_delta: float) -> void:
 	pass
 	#if Input.is_action_just_pressed("debug_0"):
 		#play("TestBeep")
+	if Input.is_action_just_pressed("toggle_music"):
+		toggle_music()
+
+func toggle_music() -> void:
+	if ost_on:
+		soundtrack_stop()
+	else:
+		soundtrack_start()
 
 func soundtrack_start() -> void:
-	# logic here to pick between songs?
-	pass
+	current_ost = "Music1"
+	#play(current_ost)
+	var sound_node: AudioStreamPlayer2D = get_node_or_null(current_ost)
+	if sound_node.stream_paused:
+		sound_node.stream_paused = false
+	else:
+		sound_node.play()
+	ost_on = true
 
 func soundtrack_stop() -> void:
-	pass
-	stop(current_ost)
+	#stop(current_ost)
+	var sound_node: AudioStreamPlayer2D = get_node_or_null(current_ost)
+	sound_node.stream_paused = true
+	ost_on = false
 
 func play(sound: NodePath):
 	# sound is the name of the node of the sound to play
 	# sound is case sensitive
-	var sound_node = get_node_or_null(sound)
+	var sound_node: AudioStreamPlayer2D = get_node_or_null(sound)
 	assert(sound_node != null, str("Sound ", sound, " does not exist"))
 	sound_node.play()
 
 func stop(sound: NodePath):
-	var sound_node = get_node_or_null(sound)
+	var sound_node: AudioStreamPlayer2D = get_node_or_null(sound)
 	assert(sound_node != null, str("Sound ", sound, " does not exist"))
 	sound_node.stop()
 
