557703d10ae205d3e85e7465a8a76c81760c8ed9
Ares <mason.hoppe@gmail.com>
2026-01-12 18:38:24 -0500

    Adjust audio levels + notes

--- source/gui/menus/title_screen.gd
+++ source/gui/menus/title_screen.gd
@@ -40,6 +40,7 @@ func _on_options_pressed() -> void:
 	#get_tree().change_scene_to_file("res://source/gui/menus/settings.tscn")
 
 func _on_credits_pressed() -> void:
+	AudioManager.play("UISelectA")
 	get_tree().change_scene_to_file("res://source/gui/menus/credits.tscn")
 
 func _on_quit_pressed() -> void:
